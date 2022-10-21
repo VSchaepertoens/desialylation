fda1d73b3d4a8c9f33b153d7e84c18254e7693e0
VSchaepertoens <vschaepertoens@protonmail.com>
2022-10-21 14:23:12 +0200

    Get dataframes for peak 2 and peak 3 to look at composition of glycans

--- main.R
+++ main.R
@@ -47,7 +47,9 @@ find_interval_mean <- function(interval) {
 # intensity in each bin
 desialylate <- function(peaks_sial,
                         peaks_desial_exp,
+                        mass_desial_peak,
                         filter_peaks = TRUE,
+                        filter_max_peaks = TRUE,
                         mass_tolerance = 5) {
   MASS_NEU5AC <- 291.256  # mass of a sialic acid
   MASS_AC <- 42.0106  # acetylation
@@ -80,38 +82,66 @@ desialylate <- function(peaks_sial,
     / mass_tolerance
   )
 
-  peaks_desial_comp %>%
-    mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean()) %>%
-    group_by(mass) %>%
-    summarise(intensity = sum(percent)) %>%
-    mutate(intensity = intensity / max(intensity) * 100)
+  # if desired, only keep MoFi hits where the computationally desialylated mass
+  # corresponds to a mass of a given peak (within the given mass_tolerance)
+  if (filter_max_peaks){
+    peaks_desial_comp %>%
+      mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean()) %>%
+      filter(near(mass, mass_desial_peak, tol = mass_tolerance))
+  }
+
+  # peaks_desial_comp %>%
+  #     mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean()) %>%
+  #     group_by(mass) %>%
+  #     summarise(intensity = sum(percent)) %>%
+  #     mutate(intensity = intensity / max(intensity) * 100)
+
 }
 
-df_desial <- desialylate(mofi_results_sial, mofi_results_desial)
+###BUG In order to run this, uncomment lines 93-97
+df_desial <- desialylate(mofi_results_sial, mofi_results_desial, filter_max_peaks = FALSE)
 
+
+## To find the masses of the desired peaks
+# sort(df_desial$intensity,decreasing=TRUE,index.return= TRUE)
+#
+# df_desial[c(108,159,167,83),]#display top 4 peaks and their masses
+#
+# max_peak2 <- df_desial[c(159),'mass']
+# max_peak3 <- df_desial[c(167),'mass']
+
+mass_peak2 = 110633.5
+mass_peak3 = 110958.5
+df_desial_peak2 <- desialylate(mofi_results_sial, mofi_results_desial,mass_desial_peak = mass_peak2)
+df_desial_peak3 <- desialylate(mofi_results_sial, mofi_results_desial,mass_desial_peak = mass_peak3)
+
+# write.table(df_desial_peak2, file = "df_desial_peak2.txt", sep = "\t",
+#             row.names = TRUE, col.names = NA)
+# write.table(df_desial_peak3, file = "df_desial_peak3.txt", sep = "\t",
+#             row.names = TRUE, col.names = NA)
 
 
 # Plot data ---------------------------------------------------------------
 
-bind_rows(
-  experimental =
-    mofi_results_desial %>%
-    group_by(peak_id) %>%
-    summarise(across(c(exp_mass, percent), first)) %>%
-    select(mass = exp_mass, intensity = percent),
-  computational =
-    df_desial %>%
-    mutate(intensity = intensity * -1),
-  .id = "desialylation"
-) %>%
-  mutate(desialylation = fct_rev(desialylation)) %>%
-  ggplot(aes(mass, 0, xend = mass, yend = intensity)) +
-  geom_segment(aes(color = desialylation)) +
-  geom_hline(yintercept = 0) +
-  xlab("mass (Da)") +
-  ylab("relative intensity (%)") +
-  theme_bw() +
-  theme(panel.grid = element_blank())
+# bind_rows(
+#   experimental =
+#     mofi_results_desial %>%
+#     group_by(peak_id) %>%
+#     summarise(across(c(exp_mass, percent), first)) %>%
+#     select(mass = exp_mass, intensity = percent),
+#   computational =
+#     df_desial %>%
+#     mutate(intensity = intensity * -1),
+#   .id = "desialylation"
+# ) %>%
+#   mutate(desialylation = fct_rev(desialylation)) %>%
+#   ggplot(aes(mass, 0, xend = mass, yend = intensity)) +
+#   geom_segment(aes(color = desialylation)) +
+#   geom_hline(yintercept = 0) +
+#   xlab("mass (Da)") +
+#   ylab("relative intensity (%)") +
+#   theme_bw() +
+#   theme(panel.grid = element_blank())
 #ggsave("desialylated_spectra_experimental_vs_computational.pdf")
 
 
