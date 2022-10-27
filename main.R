d21716627725402e035ab2870b27c37612c6de7f
VSchaepertoens <vschaepertoens@protonmail.com>
2022-10-27 12:02:12 +0200

    filters hit_score & fixes bug by assigning to peaks_desial_comp variable

--- main.R
+++ main.R
@@ -42,23 +42,25 @@ find_interval_mean <- function(interval) {
   (as.numeric(span[,2]) + as.numeric(span[,3])) / 2
 }
 
-# calculates desialylated masses, compares and matches them to experimentally
-# desialylated peaks, bins them with a given width (mass_tolerance), and sums
-# intensity in each bin
+# calculates desialylated masses, (optional) compares and matches them to experimentally
+# desialylated peaks, (optional) filters hit_scores, (optional) filters peaks of certain mass,
+# bins them with a given width (mass_tolerance), and sums intensity in each bin
 desialylate <- function(peaks_sial,
                         peaks_desial_exp,
                         mass_desial_peak,
                         filter_peaks = TRUE,
                         filter_max_peaks = TRUE,
+                        filter_hit_score = TRUE,
+                        hit_score_cutoff = 0.01,
                         mass_tolerance = 5) {
   MASS_NEU5AC <- 291.256  # mass of a sialic acid
   MASS_AC <- 42.0106  # acetylation
-
+  
   # experimental desialylation
   peaks_desial_comp <-
     peaks_sial %>%
     mutate(mass_desial = exp_mass - MASS_NEU5AC * neu5ac - MASS_AC * acetyl)
-
+  
   # if desired, only keep MoFi hits where the computationally desialylated mass
   # corresponds to a peak in the experimentally desialylated spectrum (within
   # the given mass_tolerance)
@@ -76,12 +78,22 @@ desialylate <- function(peaks_sial,
       mutate(hit_score = hit_score / sum(hit_score) * 100) %>%
       ungroup()
   }
-
+  
+  # if desired, only keep MoFi hits where hit_score > hit_score_cutoff
+  if (filter_hit_score) {
+    peaks_desial_comp <-
+      peaks_desial_comp %>%
+      filter(hit_score > hit_score_cutoff) %>%
+      group_by(peak_id) %>%
+      mutate(hit_score = hit_score / sum(hit_score) * 100) %>%
+      ungroup()
+  }
+  
   n_bins <- round(
     (max(peaks_desial_comp$mass_desial) - min(peaks_desial_comp$mass_desial))
     / mass_tolerance
   )
-
+  
   # if desired, only keep MoFi hits where the computationally desialylated mass
   # corresponds to a mass of a given peak (within the given mass_tolerance)
   if (filter_max_peaks){
@@ -89,18 +101,17 @@ desialylate <- function(peaks_sial,
       mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean()) %>%
       filter(near(mass, mass_desial_peak, tol = mass_tolerance))
   }
-
-  # peaks_desial_comp %>%
-  #     mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean()) %>%
-  #     group_by(mass) %>%
-  #     summarise(intensity = sum(percent)) %>%
-  #     mutate(intensity = intensity / max(intensity) * 100)
-
+  
+  peaks_desial_comp %>%
+    mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean())%>%
+    group_by(mass) %>%
+    summarise(intensity = sum(percent)) %>%
+    mutate(intensity = intensity / max(intensity) * 100)
+  
 }
 
-###BUG In order to run this, uncomment lines 93-97
 df_desial <- desialylate(mofi_results_sial, mofi_results_desial, filter_max_peaks = FALSE)
-
+df_desial <- desialylate(mofi_results_sial, mofi_results_desial, filter_max_peaks = FALSE, hit_score_cutoff = 0.01)
 
 ## To find the masses of the desired peaks
 # sort(df_desial$intensity,decreasing=TRUE,index.return= TRUE)
@@ -123,26 +134,26 @@ df_desial_peak3 <- desialylate(mofi_results_sial, mofi_results_desial,mass_desia
 
 # Plot data ---------------------------------------------------------------
 
-# bind_rows(
-#   experimental =
-#     mofi_results_desial %>%
-#     group_by(peak_id) %>%
-#     summarise(across(c(exp_mass, percent), first)) %>%
-#     select(mass = exp_mass, intensity = percent),
-#   computational =
-#     df_desial %>%
-#     mutate(intensity = intensity * -1),
-#   .id = "desialylation"
-# ) %>%
-#   mutate(desialylation = fct_rev(desialylation)) %>%
-#   ggplot(aes(mass, 0, xend = mass, yend = intensity)) +
-#   geom_segment(aes(color = desialylation)) +
-#   geom_hline(yintercept = 0) +
-#   xlab("mass (Da)") +
-#   ylab("relative intensity (%)") +
-#   theme_bw() +
-#   theme(panel.grid = element_blank())
-#ggsave("desialylated_spectra_experimental_vs_computational.pdf")
+bind_rows(
+  experimental =
+    mofi_results_desial %>%
+    group_by(peak_id) %>%
+    summarise(across(c(exp_mass, percent), first)) %>%
+    select(mass = exp_mass, intensity = percent),
+  computational =
+    df_desial %>%
+    mutate(intensity = intensity * -1),
+  .id = "desialylation"
+) %>%
+  mutate(desialylation = fct_rev(desialylation)) %>%
+  ggplot(aes(mass, 0, xend = mass, yend = intensity)) +
+  geom_segment(aes(color = desialylation)) +
+  geom_hline(yintercept = 0) +
+  xlab("mass (Da)") +
+  ylab("relative intensity (%)") +
+  theme_bw() +
+  theme(panel.grid = element_blank())
+ggsave("plots/desialylated_spectra_experimental_vs_computational_cutoff0.01_.pdf")
 
 
 
