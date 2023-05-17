7b8a661a0f07a43e30bcec46312254791c325add
VSchaepertoens <vschaepertoens@protonmail.com>
2023-05-17 11:50:37 +0200

    calculates fractional abundances, filters them with a cut-off

--- main.R
+++ main.R
@@ -42,14 +42,17 @@ find_interval_mean <- function(interval) {
   (as.numeric(span[,2]) + as.numeric(span[,3])) / 2
 }
 
+
 # calculates desialylated masses, (optional) compares and matches them to experimentally
-# desialylated peaks, (optional) filters hit_scores, (optional) filters peaks of certain mass,
-# bins them with a given width (mass_tolerance), and sums intensity in each bin
+# desialylated peaks, (optional) filters peaks of certain mass,(optional) filters hit_scores, 
+# (optional) filters fractional abundances, bins them with a given width (mass_tolerance), 
+# and sums intensity in each bin
 desialylate <- function(peaks_sial,
                         peaks_desial_exp,
                         mass_desial_peak,
                         filter_peaks = TRUE,
                         filter_hit_score = TRUE,
+                        filter_fract_abund = TRUE,
                         hit_score_cutoff = 0.01,
                         mass_tolerance = 5) {
   MASS_NEU5AC <- 291.256  # mass of a sialic acid
@@ -59,6 +62,8 @@ desialylate <- function(peaks_sial,
   peaks_desial_comp <-
     peaks_sial %>%
     mutate(mass_desial = exp_mass - MASS_NEU5AC * neu5ac - MASS_AC * acetyl)
+  
+  #return(peaks_desial_comp)
   
   # if desired, only keep MoFi hits where the computationally desialylated mass
   # corresponds to a peak in the experimentally desialylated spectrum (within
@@ -77,6 +82,7 @@ desialylate <- function(peaks_sial,
       mutate(hit_score = hit_score / sum(hit_score) * 100) %>%
       ungroup()
   }
+  #return(peaks_desial_comp)
   
   # if desired, only keep MoFi hits where hit_score > hit_score_cutoff
   if (filter_hit_score) {
@@ -88,6 +94,18 @@ desialylate <- function(peaks_sial,
       ungroup()
   }
   
+  
+  # if desired, calculate fractional abundance and filter 
+  if (filter_fract_abund) {
+    peaks_desial_comp <-
+      peaks_desial_comp %>%
+      group_by(peak_id) %>%
+      mutate(fractional_abundance = (hit_score * percent) / sum(percent)) %>%
+      filter(fractional_abundance > 0.01)
+  }
+  
+  #return(peaks_desial_comp)
+  
   n_bins <- round(
     (max(peaks_desial_comp$mass_desial) - min(peaks_desial_comp$mass_desial))
     / mass_tolerance
@@ -97,7 +115,7 @@ desialylate <- function(peaks_sial,
     mutate(mass = mass_desial %>% cut(n_bins) %>% find_interval_mean()) %>%
     group_by(mass) %>%
     summarise(intensity = sum(percent)) %>%
-    mutate(intensity = intensity / max(intensity) * 100)
+    mutate(intensity = intensity / sum(intensity) * 100)
   
 }
 
@@ -107,8 +125,11 @@ df_desial <- desialylate(
   mofi_results_sial,
   mofi_results_desial,
   filter_hit_score = FALSE,
-  filter_peaks = FALSE
+  filter_peaks = FALSE,
+  filter_fract_abund = FALSE
 )
+
+#write_csv(df_desial_df, "analysis/desialylated_spectra_experimental_vs_computational_unfiltered.csv")
 
 # 2. The _in silico_ desialylated masses were filtered based on their
 # correspondence with the experimentally desialylated masses and the fractional
@@ -117,20 +138,37 @@ df_desial_filtered <- desialylate(
   mofi_results_sial,
   mofi_results_desial,
   filter_hit_score = FALSE,
-  filter_peaks = TRUE
+  filter_peaks = TRUE,
+  filter_fract_abund = FALSE
 )
 
-# 3. The _in silico_ desialylated relative abundances were filtered with a
+#write_csv(df_desial_filtered_df, "analysis/desialylated_spectra_experimental_vs_computational_filtered.csv")
+
+
+# 3. The _in silico_ desialylated hit score were filtered with a
 # cut-off (range from 0.01 to 1) and afterwards the fractional abundances of the
 # glycoform annotations were normalized to 100%
-df_desial_filtered_cutoff <- desialylate(
+
+# df_desial_filtered_cutoff <- desialylate(
+#   mofi_results_sial,
+#   mofi_results_desial,
+#   filter_hit_score = TRUE,
+#   filter_peaks = TRUE,
+#   hit_score_cutoff = 0.01
+# )
+
+
+# 4. The fractional abundances were calculated for each peak
+# and then filtered with a cutoff & normalized to 100%
+df_desial_filtered_fract_abund <- desialylate(
   mofi_results_sial,
   mofi_results_desial,
-  filter_hit_score = TRUE,
+  filter_hit_score = FALSE,
   filter_peaks = TRUE,
-  hit_score_cutoff = 0.01
+  filter_fract_abund = TRUE
 )
 
+#write_csv(df_desial_filtered_fract_abund_df, "analysis/desialylated_spectra_experimental_vs_computational_filtered_frac_abund_cutoff_0.01.csv")
 
 
 # Plot data ---------------------------------------------------------------
@@ -141,7 +179,8 @@ plot_spectrum <- function(computational_data) {
       mofi_results_desial %>%
       group_by(peak_id) %>%
       summarise(across(c(exp_mass, percent), first)) %>%
-      select(mass = exp_mass, intensity = percent),
+      select(mass = exp_mass, intensity = percent) %>%
+      mutate(intensity = intensity / sum(intensity) * 100),
     computational =
       computational_data %>%
       mutate(intensity = intensity * -1),
@@ -151,18 +190,24 @@ plot_spectrum <- function(computational_data) {
     ggplot(aes(mass, 0, xend = mass, yend = intensity)) +
     geom_segment(aes(color = desialylation)) +
     geom_hline(yintercept = 0) +
+    scale_y_continuous(n.breaks = 10) +
     xlab("mass (Da)") +
-    ylab("relative intensity (%)") +
+    ylab("fractional abundance (%)") +
     theme_bw() +
     theme(panel.grid = element_blank())
+
 }
 
 
 plot_spectrum(df_desial)
-ggsave("plots/desialylated_spectra_experimental_vs_computational_unfiltered.pdf")
+ggsave("plots/revisions/desialylated_spectra_experimental_vs_computational_unfiltered.wmf")
 
 plot_spectrum(df_desial_filtered)
-ggsave("plots/desialylated_spectra_experimental_vs_computational_filtered.wmf")
+ggsave("plots/revisions/desialylated_spectra_experimental_vs_computational_filtered.pdf")
 
-plot_spectrum(df_desial_filtered_cutoff)
-ggsave("plots/desialylated_spectra_experimental_vs_computational_filtered_cutoff0.01.wmf")
+#plot_spectrum(df_desial_filtered_cutoff)
+#ggsave("plots/desialylated_spectra_experimental_vs_computational_filtered_cutoff0.01.wmf")
+
+plot_spectrum(df_desial_filtered_fract_abund)
+ggsave("plots/revisions/desialylated_spectra_experimental_vs_computational_filtered_frac_abund_cutoff_0.01.pdf")
+
